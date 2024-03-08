1116aa9bde8924bfe0d6aab708d33bc9d4e87a49
warlxy <warleysilva.dev@gmail.com>
2024-03-08 13:58:39 -0300

    inicialização do projeto

---
+++ src/main/java/br/com/warley/petvetapi/PetVetApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.warley.petvetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetVetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetVetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/warley/petvetapi/PetVetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.warley.petvetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetVetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
